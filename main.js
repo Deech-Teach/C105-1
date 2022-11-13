27b3381f7ddc2fd8a8b736a2adfd518116a09e41
Deech-Teach <76897436+Deech-Teach@users.noreply.github.com>
2022-11-13 16:56:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
-Webcam.set({
-width: 350,
-height:300,
-image_format: 'png',
-png_quality: 90
-});
-
-camera = document.getElementById("camera");
-
+      
 Webcam.attach( '#camera' );
 
-function take_snapshot(){
-Webcam.snap( function(data_uri) {
-document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
-});
+camera = document.getElementById("camera");
+      
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
 
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
 }
 
-console.log('ml5 version', ml5.version);
+  console.log('ml5 version:', ml5.version);
+  
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/vkDWmT0y6/ model.jason', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
 
-function modelLoaded(){
-console.log('model loaded');
-}
 
-function check(){
-img = document.getElementById('capture_image');
-classifier.classify(img, gotResult);
-}
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+  }
 
-function gotResult(error,result){
-if(error){
-console.error(error);
-} else {
-console.log(results);
-document.getElementById("reslut_object_name").innerHTML = results[0].label;
-document.getElementById("reslut_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-}
+
+
+function gotResult(error, results) {
+ 
+  if (error) {
+    console.error(error);
+  } else {
+ 
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
 }
